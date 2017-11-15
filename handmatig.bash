ab3e23256f76f4a98d73bf81a056a51f87b8468a
unknown <angelanieboer@gmail.com>
2017-11-15 12:06:14 +0100

    Laatste commit voordat ik gek word

--- handmatig.bash
+++ handmatig.bash
@@ -4,12 +4,16 @@
 #Toevoegen aan nagios:
 sudo sh add-to-nagios.sh hname ip
 #salt-key accepteren
-sudo salt-key -A -y
+sudo salt-key -d minion* -y
 #salt state.apply
-sudo salt '*' state.apply
+sudo salt 'minion*' state.apply
 
 #Dingen om te doen na het verwijderen van een instance
-#salt-key verwijderen
+#salt-key verwijderen (alle met hostname beginnend met minion)
 sudo salt-key -d minion* -y
 #verwijderen uit docker swarm
 docker swarm rm [ID]
+#verwijderen uit nagios
+sudo rm /usr/local/nagios/etc/servers/((minion nr).cfg
+#nagios herstarten
+sudo systemctl restart nagios.service
--- startup.sh
+++ startup.sh
@@ -4,37 +4,33 @@
 # Setting the right timezone
 timedatectl set-timezone 'Europe/Amsterdam'
 
+#updating sources
 #Getting the saltstack public key to add to apt
 wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
-
 #Copying the repo to a source list
 sh -c 'echo "deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main" >> /etc/apt/sources.list.d/saltstack.list'
-
 #updating apt so it includes the new source
 apt-get update -y
 
+#installing all the tings
 #installing the minion
 apt-get install salt-minion -y
-
-#giving the minion the master's IP address
-sed -i -e 's/#master: salt/master: 10.5.1.60/' /etc/salt/minion
-
-#salt-minion needs to be restarted to see the changes in config
-systemctl restart salt-minion
-
-
-#adding the minion to the monitor
 #installing nagios
 apt-get install nagios-nrpe-server nagios-plugins -y
 
+#configuring all the tings
+#giving the minion the master's IP address
+sed -i -e 's/#master: salt/master: 10.5.1.60/' /etc/salt/minion
 #edit the config to point to the monitor server
 sed -i -e 's/#server_address=127.0.0.1/server_address=10.5.1.60/' /etc/nagios/nrpe.cfg
-
-#restart the service to find changes
-service nagios-nrpe-server restart
-
 #adding the logserver to rsyslog.conf
 echo "*.*       @@10.5.1.60:514" >> /etc/rsyslog.conf
 
+
+#restarting all the tings
+#restart the service to find changes
+service nagios-nrpe-server restart
 #restarting the logging services
 systemctl restart rsyslog
+#salt-minion needs to be restarted to see the changes in config
+systemctl restart salt-minion
